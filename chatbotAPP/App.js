a66cc9ad41ab54ccc2f1ce11dbe4db35825ac699
yh960508 <shine_koko@naver.com>
2020-07-01 20:20:41 +0900

    "사용자말에 답변 추가"

--- chatbotAPP/App.js
+++ chatbotAPP/App.js
@@ -1,18 +1,20 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat'
+import {Alert} from 'react-native'
+import { GiftedChat, SystemMessage } from 'react-native-gifted-chat'
 
 export default function App() {
+  var idCount = 1;
   const [messages, setMessages] = useState([]);
-
+  
   useEffect(() => {
     setMessages([
       {
         _id: 1,
-        text: '안녕 세종',
+        text: '안녕하세요. 세종대학교 무엇이든물어봇입니다. 학번이 어떻게 되시나요?',
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'React Native',
+          name: 'SejongBot',
           avatar: 'https://placeimg.com/140/140/any',
         },
       },
@@ -21,15 +23,31 @@ export default function App() {
 
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    onReceive('응 나도 몰라')
+  }, [])
+  
+  const onReceive = useCallback(textt => {
+    idCount = idCount + 1,
+    setMessages(previousMessages => GiftedChat.append(previousMessages, [
+      {
+        _id: idCount,
+        text: textt,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'SejongBot',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]))
   }, [])
 
   return (
     <GiftedChat
+      placeholder = '메세지를 입력하세요'
       messages={messages}
       onSend={messages => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
     />
   );
 }
+
